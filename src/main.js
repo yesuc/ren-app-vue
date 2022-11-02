02b4413bb6cf556b29110b3e1f888b3e13e59d1a
Yesu Carter <31628681+yesuc@users.noreply.github.com>
2022-11-02 14:14:44 -0700

    Fix loadFont import in main

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 import { createApp } from "vue";
 import vuetify from "ren-app-vue/src/plugins/vuetify";
-import { loadFonts } from "ren-app-vue/src/plugins/webfontloader";
+import loadFonts from "ren-app-vue/src/plugins/webfontloader";
 import App from "ren-app-vue/src/components/core/App";
 import store from "ren-app-vue/src/store/index";
 
